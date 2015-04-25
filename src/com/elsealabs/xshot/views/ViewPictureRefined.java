a12bc0d4be44c1a195790028aa33060004c132dc
Connor Elsea <connorelsea@gmail.com>
2015-04-24 20:12:47 -0500

    Refactored PanelImage into own class

--- src/com/elsealabs/xshot/views/ViewPictureRefined.java
+++ src/com/elsealabs/xshot/views/ViewPictureRefined.java
@@ -4,9 +4,10 @@ import com.elsealabs.xshot.graphics.Capture;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.geom.AffineTransform;
 
 /**
  * PictureView.java
@@ -19,11 +20,13 @@ public class ViewPictureRefined extends JFrame {
 
     private JPanel bar;
     private JPanel container;
-    private JPanel imagePanel;
+    private PanelImage panelImage;
 
     private JScrollPane scrollPane;
 
     private Capture capture;
+
+    private Dimension prevWindowSize;
 
     public ViewPictureRefined(Capture capture)
     {
@@ -35,73 +38,51 @@ public class ViewPictureRefined extends JFrame {
 
     public void build()
     {
-        setTitle(title);
-
+        // Set size of window
         setSize(
                 (int) capture.getOriginalBounds().getWidth() + 200,
                 (int) capture.getOriginalBounds().getHeight() + barHeight + 200
         );
+
+        // Store the initial size to use  when  window
+        // is being resized. If the size changes, then
+        // the program knows the window has been resized.
+        prevWindowSize = getSize();
 
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         setLookAndFeel();
 
         getContentPane().setLayout(new BorderLayout());
 
+        // Top bar of the program. Has feature buttons.
         bar = new JPanel();
         bar.setLayout(new GridBagLayout());
         bar.setPreferredSize(new Dimension(barHeight, barHeight));
         add(bar, BorderLayout.NORTH);
 
+        // Container for easier maniuplation of the scroll pane.
         container = new JPanel();
         container.setLayout(new BorderLayout());
         add(container, BorderLayout.CENTER);
 
+        // Scroll pane with always-on scroll bars
         scrollPane = new JScrollPane();
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         container.add(scrollPane, BorderLayout.CENTER);
 
-        imagePanel = new JPanel()
-        {
-            public void paint(Graphics gd)
-            {
-                Graphics2D g = (Graphics2D) gd;
-
-                g.setColor(Color.WHITE);
-                g.fillRect(0, 0, getWidth(), getHeight());
-
-                // DRAW IMAGE
-
-                int imageX = (getWidth() / 2)  - (capture.getBoundedImage().getWidth() / 2);
-                int imageY = (getHeight() / 2) - (capture.getBoundedImage().getHeight() / 2);
-
-                int padding_x = (int) (scrollPane.getWidth() - capture.getUpdatedBounds().getWidth()) / 2;
-                int padding_y = (int) (scrollPane.getHeight() - capture.getUpdatedBounds().getHeight()) / 2;
-
-                scrollPane.getViewport().setViewPosition(
-                        new Point(
-                                (int) imageX - padding_x,
-                                (int) imageY - padding_y
-                        )
-                );
-
-                g.drawImage(
-                        capture.getBoundedImage().getBufferedImage(),
-                        imageX,
-                        imageY,
-                        null
-                );
-            }
-        };
-        imagePanel.setBorder(null);
-        imagePanel.setPreferredSize(
+        // Customized image panel
+        panelImage = new PanelImage(scrollPane, capture);
+        panelImage.setBorder(null);
+        panelImage.setPreferredSize(
                 new Dimension(
                         capture.getBoundedImage().getWidth() + 500,
                         capture.getBoundedImage().getHeight() + 500
                 )
         );
-        scrollPane.getViewport().add(imagePanel);
+        scrollPane.getViewport().add(panelImage);
 
+        setTitle(title);
         setLocationRelativeTo(null);
         setVisible(true);
     }
@@ -115,6 +96,15 @@ public class ViewPictureRefined extends JFrame {
             public void windowClosing(WindowEvent e)
             {
                 super.windowClosing(e);
+            }
+
+        });
+
+        addComponentListener(new ComponentAdapter() {
+
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
             }
 
         });
