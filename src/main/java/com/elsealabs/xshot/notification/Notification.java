40fc26c1d35aef96f75a8c6ac407e5c92ad78a91
Connor Elsea <connorelsea@gmail.com>
2015-05-27 13:31:45 -0500

    Revert "The quest for more beautiful code: Re-organizing everything."
    This reverts commit 493bb82da9010e8f230c8e7c950b21ab022cd8a8.

---
+++ src/main/java/com/elsealabs/xshot/notification/Notification.java
@@ -0,0 +1,16 @@
+package com.elsealabs.xshot.notification;
+
+/**
+ * Notification.java
+ *
+ * An implementation of any kind of pop-up information  of  notification
+ * that has the ability to contain data and display itself for a certain
+ * period of time or until a certain interaction.
+ */
+public class Notification {
+    // TODO: Notification window that holds a content container panel
+    // TODO: Ability to display over everything without taking focus
+    // TODO: Timer ability
+    // TODO: Placement at a point on either screen
+    // TODO: Possibility of this being a base class that only holds the Notification's data, extensions such as NativeNotification and SwingNotification would exist
+}
---
+++ src/main/java/com/elsealabs/xshot/views/ViewScreens.java
@@ -0,0 +1,8 @@
+package com.elsealabs.xshot.views;
+
+/**
+ * View to show screens and let user choose.. Returns an array of screens.
+ */
+public class ViewScreens {
+
+}
